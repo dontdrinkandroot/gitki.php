c732d5139e1c64004d675843a672c601018537f9
Philip Washington Sorst <philip@sorst.net>
2014-07-17 16:59:40 +0200

    Adding real logged out page, closes #14

--- src/Net/Dontdrinkandroot/Gitki/BaseBundle/Controller/DefaultController.php
+++ src/Net/Dontdrinkandroot/Gitki/BaseBundle/Controller/DefaultController.php
@@ -7,6 +7,12 @@ class DefaultController extends BaseController
     public function indexAction()
     {
         $history = $this->getWikiService()->getHistory(20);
+
         return $this->render('DdrGitkiBaseBundle:Default:index.html.twig', array('history' => $history));
+    }
+
+    public function loggedoutAction()
+    {
+        return $this->render('DdrGitkiBaseBundle:Default:loggedout.html.twig');
     }
 }
