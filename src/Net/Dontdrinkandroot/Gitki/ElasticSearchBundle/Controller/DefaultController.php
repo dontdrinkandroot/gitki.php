c62f5719a87bcb481a83fde00455791b1f58051d
Philip Washington Sorst <philip@sorst.net>
2014-07-21 00:20:10 +0200

    Implementing simple search, refs #23

---
+++ src/Net/Dontdrinkandroot/Gitki/ElasticSearchBundle/Controller/DefaultController.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Net\Dontdrinkandroot\Gitki\ElasticSearchBundle\Controller;
+
+
+use Net\Dontdrinkandroot\Gitki\BaseBundle\Controller\BaseController;
+use Net\Dontdrinkandroot\Gitki\ElasticSearchBundle\Repository\ElasticSearchRepository;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends BaseController
+{
+
+    public function searchAction(Request $request)
+    {
+        $form = $this->createFormBuilder(null, array('csrf_protection' => false))
+            ->add('searchString', 'text', array('label' => 'Text'))
+            ->add('search', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $searchString = $form->get('searchString')->getData();
+            $paths = $this->getElasticSearchRepository()->searchMarkdownDocuments($searchString);
+
+            return $this->render(
+                'DdrGitkiElasticSearchBundle:Default:search.html.twig',
+                array('searchString' => $searchString, 'paths' => $paths)
+            );
+        }
+    }
+
+    /**
+     * @return ElasticSearchRepository
+     */
+    protected function getElasticSearchRepository()
+    {
+        return $this->get('ddr.gitki.repository.elasticsearch');
+    }
+
+} 
