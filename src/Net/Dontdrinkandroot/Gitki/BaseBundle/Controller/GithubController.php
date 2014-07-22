080e801b4247e84a626e00c86738e39d680ff495
Philip Washington Sorst <philip@sorst.net>
2014-07-22 20:36:29 +0200

    Making issue create page work, closes #25

--- src/Net/Dontdrinkandroot/Gitki/BaseBundle/Controller/GithubController.php
+++ src/Net/Dontdrinkandroot/Gitki/BaseBundle/Controller/GithubController.php
@@ -4,21 +4,20 @@
 namespace Net\Dontdrinkandroot\Gitki\BaseBundle\Controller;
 
 
+use Github\Api\Issue;
 use Github\Client;
+use Github\HttpClient\CachedHttpClient;
 use Symfony\Component\HttpFoundation\Request;
 
 class GithubController extends BaseController
 {
 
-    const ORGANIZATION = 'dontdrinkandroot';
+    const ORGANIZATION = 'philipsorst';
     const PROJECT = 'gitki.php';
 
     public function issuesAction()
     {
-        $client = new Client();
-        /* @var \Github\Api\Issue $issue */
-        $issue = $client->api('issue');
-        $issues = $issue->all(self::ORGANIZATION, self::PROJECT);
+        $issues = $this->getIssueApi()->all(self::ORGANIZATION, self::PROJECT);
 
         return $this->render('DdrGitkiBaseBundle:Github:issues.html.twig', array('issues' => $issues));
     }
@@ -38,25 +37,45 @@ class GithubController extends BaseController
         if ($form->isSubmitted()) {
 
             if ($form->isValid()) {
+
                 $title = $form->get('title')->getData();
                 $body = $form->get('body')->getData();
+                if (null === $body) {
+                    $body = "";
+                }
 
-                $client = new Client();
                 $accessToken = $this->getUser()->getAccessToken();
-                $client->authenticate($accessToken, Client::AUTH_HTTP_TOKEN);
-                /* @var \Github\Api\Issue $issue */
-                $issue = $client->api('issue');
-                $params = array(
-                    'title' => $title,
-                    'body' => $body
+                $this->getIssueApi($accessToken)->create(
+                    self::ORGANIZATION,
+                    self::PROJECT,
+                    array(
+                        'title' => $title,
+                        'body' => $body
+                    )
                 );
-                $issue->create(self::ORGANIZATION, self::PROJECT, $params);
 
-                $this->redirect($this->generateUrl('ddr_gitki_github_issues'));
+                return $this->redirect($this->generateUrl('ddr_gitki_github_issues'));
             }
         }
 
         return $this->render('DdrGitkiBaseBundle:Github:issue_create.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @param string|null $accessToken
+     * @return Issue
+     */
+    protected function getIssueApi($accessToken = null)
+    {
+        $client = new Client(
+            new CachedHttpClient(array('cache_dir' => '/tmp/github-api-cache'))
+        );
+        if (null !== $accessToken) {
+            $client->authenticate($accessToken, "", Client::AUTH_HTTP_PASSWORD);
+        }
+        $issueApi = $client->api('issue');
+
+        return $issueApi;
     }
 
 
