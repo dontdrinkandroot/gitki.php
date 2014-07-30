796b3086a2e98db95a5e19f3350100de00e8ee1a
Philip Sorst <philip@sorst.net>
2014-07-30 15:29:48 +0200

    Adding ability to set default oauth login provider, closes #37

--- src/Net/Dontdrinkandroot/Gitki/BaseBundle/DependencyInjection/DdrGitkiExtension.php
+++ src/Net/Dontdrinkandroot/Gitki/BaseBundle/DependencyInjection/DdrGitkiExtension.php
@@ -34,25 +34,31 @@ class DdrGitkiExtension extends Extension implements PrependExtensionInterface
         $twigConfig['globals']['ddr_gitki_show_toc'] = $config['twig']['show_toc'];
         $twigConfig['globals']['ddr_gitki_show_breadcrumbs'] = $config['twig']['show_breadcrumbs'];
 
-        if (isset($config['oauth']['github'])) {
+        if (isset($config['oauth']['default_provider'])) {
+            $securityConfig['firewalls']['secured_area']['oauth']['login_path'] = '/connect/' . $config['oauth']['default_provider'];
+        } else {
+            $securityConfig['firewalls']['secured_area']['oauth']['login_path'] = '/login';
+        }
 
-            $gitHubConfig = $config['oauth']['github'];
+        if (isset($config['oauth']['providers']['github'])) {
+
+            $googleConfig = $config['oauth']['providers']['github'];
             $hwiOauthConfig['resource_owners']['github'] = array(
                 'type' => 'github',
-                'client_id' => $gitHubConfig['client_id'],
-                'client_secret' => $gitHubConfig['secret'],
+                'client_id' => $googleConfig['client_id'],
+                'client_secret' => $googleConfig['secret'],
                 'scope' => 'user:email,public_repo'
             );
             $securityConfig['firewalls']['secured_area']['oauth']['resource_owners']['github'] = '/login/check-github';
         }
 
-        if (isset($config['oauth']['google'])) {
+        if (isset($config['oauth']['providers']['google'])) {
 
-            $gitHubConfig = $config['oauth']['google'];
+            $googleConfig = $config['oauth']['providers']['google'];
             $hwiOauthConfig['resource_owners']['google'] = array(
                 'type' => 'google',
-                'client_id' => $gitHubConfig['client_id'],
-                'client_secret' => $gitHubConfig['secret'],
+                'client_id' => $googleConfig['client_id'],
+                'client_secret' => $googleConfig['secret'],
                 'scope' => 'email profile'
             );
             $securityConfig['firewalls']['secured_area']['oauth']['resource_owners']['google'] = '/login/check-google';
@@ -89,20 +95,38 @@ class DdrGitkiExtension extends Extension implements PrependExtensionInterface
         $container->setParameter('ddr_gitki_base.repository_path', $config['repository_path']);
         $container->setParameter('ddr_gitki_base.name', $config['name']);
 
-        if (isset($config['oauth']) && isset($config['oauth']['github'])) {
-            $container->setParameter('ddr_gitki_base.github_users_admin', $config['oauth']['github']['users_admin']);
-            $container->setParameter('ddr_gitki_base.github_users_commit', $config['oauth']['github']['users_commit']);
-            $container->setParameter('ddr_gitki_base.github_users_watch', $config['oauth']['github']['users_watch']);
+        if (isset($config['oauth']['providers']['github'])) {
+            $container->setParameter(
+                'ddr_gitki_base.github_users_admin',
+                $config['oauth']['providers']['github']['users_admin']
+            );
+            $container->setParameter(
+                'ddr_gitki_base.github_users_commit',
+                $config['oauth']['providers']['github']['users_commit']
+            );
+            $container->setParameter(
+                'ddr_gitki_base.github_users_watch',
+                $config['oauth']['providers']['github']['users_watch']
+            );
         } else {
             $container->setParameter('ddr_gitki_base.github_users_admin', array());
             $container->setParameter('ddr_gitki_base.github_users_commit', array());
             $container->setParameter('ddr_gitki_base.github_users_watch', array());
         }
 
-        if (isset($config['oauth']) && isset($config['oauth']['google'])) {
-            $container->setParameter('ddr_gitki_base.google_users_admin', $config['oauth']['google']['users_admin']);
-            $container->setParameter('ddr_gitki_base.google_users_commit', $config['oauth']['google']['users_commit']);
-            $container->setParameter('ddr_gitki_base.google_users_watch', $config['oauth']['google']['users_watch']);
+        if (isset($config['oauth']['providers']['google'])) {
+            $container->setParameter(
+                'ddr_gitki_base.google_users_admin',
+                $config['oauth']['providers']['google']['users_admin']
+            );
+            $container->setParameter(
+                'ddr_gitki_base.google_users_commit',
+                $config['oauth']['providers']['google']['users_commit']
+            );
+            $container->setParameter(
+                'ddr_gitki_base.google_users_watch',
+                $config['oauth']['providers']['google']['users_watch']
+            );
         } else {
             $container->setParameter('ddr_gitki_base.google_users_admin', array());
             $container->setParameter('ddr_gitki_base.google_users_commit', array());
--- update-prod.sh
+++ update-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull && composer install --no-dev && bin/console cache:clear --env=prod && bin/console assetic:dump --env=prod
+git pull && composer install && bin/console cache:clear --env=prod && bin/console assetic:dump --env=prod
