858dd052cccc813c9e8871540b1fcbdba0728105
Philip Washington Sorst <philip@sorst.net>
2014-08-18 19:35:25 +0200

    style.css is now generated during installation as long as we are in alpha, refs #30

--- update-dev.sh
+++ update-dev.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 git pull && composer update && bin/console assetic:dump
+lessc -x src/Net/Dontdrinkandroot/Gitki/BaseBundle/Resources/public/css/style.less > src/Net/Dontdrinkandroot/Gitki/BaseBundle/Resources/public/css/style.css
+
