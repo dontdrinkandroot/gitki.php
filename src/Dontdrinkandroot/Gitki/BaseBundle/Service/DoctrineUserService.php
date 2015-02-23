fb98be8484726daa3bc2b34c8acf401dcebc1f06
Philip Washington Sorst <philip@sorst.net>
2015-02-23 23:19:14 +0100

    Refactoring DoctrineUserService, refs #80

--- src/Dontdrinkandroot/Gitki/BaseBundle/Service/DoctrineUserService.php
+++ src/Dontdrinkandroot/Gitki/BaseBundle/Service/DoctrineUserService.php
@@ -37,63 +37,6 @@ class DoctrineUserService implements UserService
     }
 
     /**
-     * @param string $email
-     *
-     * @return User|null
-     */
-    public function findByEmail($email)
-    {
-        $users = $this->userRepository->findBy(['email' => $email]);
-        if (empty($users)) {
-            return null;
-        }
-
-        if (count($users) > 1) {
-            throw new \Exception('Found more than one user for email \"' . $email . '\"');
-        }
-
-        return $users[0];
-    }
-
-    /**
-     * @param string $login
-     *
-     * @return User|null
-     */
-    public function findByGitHubLogin($login)
-    {
-        $users = $this->userRepository->findBy(['githubLogin' => $login]);
-        if (empty($users)) {
-            return null;
-        }
-
-        if (count($users) > 1) {
-            throw new \Exception('Found more than one user for github login \"' . $login . '\"');
-        }
-
-        return $users[0];
-    }
-
-    /**
-     * @param string $login
-     *
-     * @return User|null
-     */
-    public function findByGoogleLogin($login)
-    {
-        $users = $this->userRepository->findBy(['googleLogin' => $login]);
-        if (empty($users)) {
-            return null;
-        }
-
-        if (count($users) > 1) {
-            throw new \Exception('Found more than one user for google login \"' . $login . '\"');
-        }
-
-        return $users[0];
-    }
-
-    /**
      * @return User[]
      */
     public function listUsers()
--- src/Dontdrinkandroot/Gitki/BaseBundle/Service/UserService.php
+++ src/Dontdrinkandroot/Gitki/BaseBundle/Service/UserService.php
@@ -9,27 +9,6 @@ interface UserService
 {
 
     /**
-     * @param string $email
-     *
-     * @return User|null
-     */
-    public function findByEmail($email);
-
-    /**
-     * @param string $login
-     *
-     * @return User|null
-     */
-    public function findByGitHubLogin($login);
-
-    /**
-     * @param string $login
-     *
-     * @return User|null
-     */
-    public function findByGoogleLogin($login);
-
-    /**
      * @return User[]
      */
     public function listUsers();
