6326744afbb1528b50e55c82a95ca43f2fe6197f
Philip Washington Sorst <philip@sorst.net>
2018-03-05 12:02:49 +0100

    Setting up Webpcak packages

---
+++ assets/js/external-links.js
@@ -0,0 +1,3 @@
+var jQuery = require('jquery');
+jQuery('a[rel~=external]').attr('target', '_blank');
+jQuery('a[rel~=newtab]').attr('target', '_blank');
--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,15 @@ Encore
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
-// uncomment to create hashed filenames (e.g. app.abc123.css)
-// .enableVersioning(Encore.isProduction())
+    // uncomment to create hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning(Encore.isProduction())
 
-// uncomment to define the assets of the project
-// .addEntry('js/app', './assets/js/app.js')
-// .addStyleEntry('css/app', './assets/css/app.scss')
+    // uncomment to define the assets of the project
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/css/app.scss')
 
-// uncomment if you use Sass/SCSS files
-// .enableSassLoader()
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader()
 
 // uncomment for legacy applications that require $/jQuery as a global variable
 // .autoProvidejQuery()
