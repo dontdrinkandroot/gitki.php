9021339b031782da51b024cc33cf4e7172413d83
Philip Washington Sorst <philip@sorst.net>
2019-10-24 16:22:33 +0200

    PHP >= 7.2

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
-use Liip\TestFixturesBundle\LiipTestFixturesBundle;
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class                  => ['all' => true],
@@ -18,6 +16,6 @@ return [
     Dontdrinkandroot\GitkiBundle\DdrGitkiBundle::class                   => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class                                  => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class         => ['dev' => true, 'test' => true],
-    LiipTestFixturesBundle::class                                        => ['dev' => true, 'test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class                => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class               => ['all' => true],
 ];
